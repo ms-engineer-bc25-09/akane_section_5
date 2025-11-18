a23d2d2b3985d599aa96c21f831f88300d2576e0
akanemem <minami.pig.cat.dog.peekaboo@gmail.com>
2025-11-18 20:44:52 +0900

    tests\setup.tsファイルからsupertest用のインポート文を削除

--- server/tests/setup.ts
+++ server/tests/setup.ts
@@ -1,22 +1,16 @@
-import { beforeAll, afterAll } from "vitest"; // ⑤ vitest から beforeAll と afterAll をインポート
-import { PrismaClient } from "@prisma/client"; // ① PrismaClient をインポート
-import { execSync } from "child_process"; // ② child_process モジュールをインポート
-import { app } from "../src/index"; // ③ app をインポート
-import request from "supertest"; // ④ supertest をインポート
+import { beforeAll, afterAll } from "vitest";
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "child_process";
 
 // PrismaClient インスタンスをエクスポート
 export const prisma = new PrismaClient();
 
-// ⑤ beforeAll と afterAll を使って、テスト前後のセットアップとクリーンアップを実行
+// テスト前後のDBリセットと接続管理
 beforeAll(async () => {
-  // DBリセット
   execSync("npx prisma migrate reset --force", { stdio: "inherit" });
-
-  // seed投入
   execSync("npx prisma db seed", { stdio: "inherit" });
 });
 
-// DB接続を切断
 afterAll(async () => {
   await prisma.$disconnect();
 });
